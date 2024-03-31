ac54a642066d0faaa2dbe49bf9c7ec01293b2fac
ARRAY-LIST <1348909544@qq.com>
2024-03-31 11:29:51 +0800

    add element-plus

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import {useRouter, useRoute} from "vue-router";
+import {getCurrentInstance} from 'vue'
 
 const router = useRouter()
 const route = useRoute()
 
 console.log(route.params)
+
+const { proxy }: any = getCurrentInstance()
+proxy.$message({
+  message: 'Congrats, this is a success message.',
+  type: 'success',
+  plain: true,
+})
 </script>
 
 <template>
   <h1>首页</h1>
   <div class="text-cyan-300 hover:cursor-pointer" @click="() => router.push({path: '/mine', query: {id: 1}})">个人中心
   </div>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped>
